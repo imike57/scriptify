02e1cf48d96319eaac3059ba306a30c7bc7bbfbf
Jordan Skousen <jordan.skousen@kane-group.com>
2023-06-08 17:47:59 -0600

    Add global scripts

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,54 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as os from 'os';
 
-
+interface ScriptFile {
+	name: string,
+	uri: string,
+}
 
 // const terminal = vscode.window.createTerminal('Scriptify');
-const outputChanel = vscode.window.createOutputChannel("Scriptify");
+const outputChanel = vscode.window.createOutputChannel('Scriptify');
 
-function createScriptFile() {
+function getGlobalFolder(): string {
+	return vscode.workspace.getConfiguration('scriptify').get<string>('globalFolderLocation') || os.tmpdir();
+}
+
+function getWorkspaceFolder(ignoreErrors = false): string | null {
+	const {workspaceFolders} = vscode.workspace;
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		if (!ignoreErrors) {
+			throw new Error('No folder open. Add a folder to your workspace, or create a global script file instead.');
+		}
+		return null;
+	}
+	if (workspaceFolders.length > 1) {
+		if (!ignoreErrors) {
+			throw new Error('Local script files cannot be created on workspaces with multiple folders open. Create a global script file instead.');
+		}
+		return null;
+	}
+	return workspaceFolders[0].uri.fsPath;
+}
+ 
+function createScriptFile(createGlobally = false) {
+	let workspaceFolder : string;
+	if (!createGlobally) {
+		try {
+			workspaceFolder = getWorkspaceFolder() || "";
+		}
+		catch (err) {
+			return vscode.window.showErrorMessage(`${err}`);
+		}
+	}
+	
 	vscode.window.showInputBox({
 		prompt: "Enter script name"
 	}).then(scriptName => {
 		if (scriptName) {
-			const scriptFolder = path.join(vscode.workspace.rootPath || '', '.scriptify');
+		  const parentPath = createGlobally ? getGlobalFolder() : workspaceFolder;
+			const scriptFolder = path.join(parentPath, '.scriptify');
 			if (!fs.existsSync(scriptFolder)) {
 				fs.mkdirSync(scriptFolder);
 			}
@@ -29,45 +65,62 @@ function transform(value) {
 module.exports = transform;
 `;
 
-			fs.writeFileSync(scriptPath, scriptContent);
+			fs.writeFileSync(scriptPath, scriptContent); 
+
+			vscode.workspace.openTextDocument(scriptPath).then(doc => vscode.window.showTextDocument(doc));
 
 			vscode.window.showInformationMessage(`Script "${scriptName}" created successfully.`);
 		}
 	});
 }
 
+function createGlobalScriptFile() {
+	createScriptFile(true);
+}
+
+function getScriptFiles(parentPath: string) : Array<ScriptFile> {
+	const scriptFolder = path.join(parentPath || '', '.scriptify');
+	if (!fs.existsSync(scriptFolder)) {
+		return [];
+	}
+
+	return fs.readdirSync(scriptFolder).map(f => ({
+		name: f.replace(/\.js$/g, ""),
+		uri: path.join(scriptFolder, f),
+	}));
+}
 
 function applyScript() {
-	const scriptFolder = path.join(vscode.workspace.rootPath || '', '.scriptify');
-	if (!fs.existsSync(scriptFolder)) {
+	let files: Array<ScriptFile> = [];
+	try {
+		files = getScriptFiles(getWorkspaceFolder(true) || "")
+			.concat(getScriptFiles(getGlobalFolder()));
+	}
+	catch (err) {
+		return vscode.window.showErrorMessage(`${err}`);
+	}
+
+	if (files.length === 0) {
 		vscode.window.showErrorMessage('No scripts found.');
 		return;
 	}
 
-	fs.readdir(scriptFolder, (err, files) => {
-		if (err) {
-			vscode.window.showErrorMessage('Failed to read scripts folder.');
-			return;
-		}
-
-		if (files.length === 0) {
-			vscode.window.showErrorMessage('No scripts found.');
-			return;
-		}
-
-		vscode.window.showQuickPick(files, {
-			placeHolder: 'Select a script to apply'
-		}).then(selectedScript => {
-			if (selectedScript) {
-				try {
-					
-				const scriptPath = path.join(scriptFolder, selectedScript);
+	vscode.window.showQuickPick(files.map(f => f.name), {
+		placeHolder: 'Select a script to apply'
+	}).then(selectedScriptName => {
+		if (selectedScriptName) {
+			try {
+				const file = files.find(f => f.name === selectedScriptName);
+				if (!file) {
+					vscode.window.showErrorMessage(`Script "${selectedScriptName}" not found.`);
+					return;
+				}
+				const scriptPath = file.uri;
 				console.log("scriptPath", scriptPath);
 
 				var scriptString = fs.readFileSync(scriptPath, "utf-8");
 				const transform = eval(scriptString);
 				const editor = vscode.window.activeTextEditor;
-
 
 				if (editor) {
 					const document = editor.document;
@@ -89,14 +142,12 @@ function applyScript() {
 						}
 					});
 				}
-			
-				} catch (error:any) {
-					console.log(error);
-					outputChanel.show(true);
-					outputChanel.append(error);
-				}
+			} catch (error:any) {
+				console.log(error);
+				outputChanel.show(true);
+				outputChanel.append(error);
 			}
-		});
+		}
 	});
 }
 
@@ -108,6 +159,7 @@ function applyScript() {
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('scriptify.createScript', createScriptFile),
+		vscode.commands.registerCommand('scriptify.createGlobalScript', createGlobalScriptFile),
 		vscode.commands.registerCommand('scriptify.applyScript', applyScript)
 	);
 }
