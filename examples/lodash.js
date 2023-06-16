48db373b5c2c5cd9e667d57d161c1a285e772820
MATHIAS Mickael <mickael.mathias@ext.europarl.europa.eu>
2023-06-16 08:12:34 +0200

    Update lodash script to use any fn

--- examples/lodash.js
+++ examples/lodash.js
@@ -1,6 +1,93 @@
 const _ = _require('lodash');
+
 function transform(value) {
-  return _.kebabCase(value);
+
+    return new Promise((resolve, reject) => {
+
+        const lodashFns = Object.entries(_).filter(el => typeof el[1] === "function");
+        const keys = lodashFns.map(el => el[0]);
+        vscode.window.showQuickPick(keys).then(async val => {
+            outputChannel.clear();
+            outputChannel.show(true);
+
+            outputChannel.append(`${val}\r`);
+
+            const getFnParams = (fn) => {
+                return fn.toString()
+                    .match(/\(.*?\)/)[0]
+                    .replace(/[()]/g, '')
+                    .split(',')
+                    .map(param => param.trim());
+            };
+            const fn = _[val];
+            const fnParams = getFnParams(fn);
+            const userParams = [];
+
+            for (const [index, param] of fnParams.entries()) {
+                await vscode.window.showInputBox({
+                  prompt: `Please enter a value for the param "${param}"`,
+                  value: index === 0 ? value : ""
+                }).then(val => {
+                    userParams.push(parseData(val));
+                });
+            }
+
+            outputChannel.append("Your params: ");
+            outputChannel.append(userParams);
+            outputChannel.append("\r");
+
+            const result = fn(...userParams);
+            outputChannel.append("Result: ");
+            outputChannel.append(result);
+
+            if (typeof result === "object") {
+              resolve(JSON.stringify(fn.apply(null, userParams ))) ;
+
+            } else {
+              resolve(fn.apply(null, userParams )) ;
+
+            }
+
+        });
+
+
+    });
 }
+
+
+function parseData(data) {
+
+  if (data === "") {
+    return undefined;
+  } else if (data === 'undefined') {
+    return undefined;
+  } else if (data === 'null') {
+    return null;
+  } else if (data === 'true') {
+    return true;
+  } else if (data === 'false') {
+    return false;
+  } else if (!isNaN(Number(data))) {
+    return Number(data);
+  } else if (data.startsWith('"') && data.endsWith('"')) {
+    return data.slice(1, -1);
+  } else if (data.startsWith('[') && data.endsWith(']')) {
+    try {
+      return eval(data);
+    } catch (error) {
+      return data;
+    }
+  } else if (data.startsWith('{') && data.endsWith('}')) {
+    try {
+      return eval(data);
+    } catch (error) {
+      return data;
+    }
+  } else {
+    return data;
+  }
+}
+
+
 
 module.exports = transform;
