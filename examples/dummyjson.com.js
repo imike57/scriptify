975f4ebfffdd14da51d6d91a02bfb472ca0721b3
MATHIAS Mickael <mickael.mathias@ext.europarl.europa.eu>
2023-06-17 11:17:11 +0200

    dummyjson : fix cancel selection

--- examples/dummyjson.com.js
+++ examples/dummyjson.com.js
@@ -12,67 +12,76 @@ function transform(value) {
       placeHolder: 'Select an item type.'
     }).then(selectedItem => {
 
-      const pickOptions = [
-        `get all`,
-        `get one`
-      ];
+      if (selectedItem) {
 
-      /**
-       * Displays a quick pick menu to select an option.
-       */
-      vscode.window.showQuickPick(pickOptions, {
-        placeHolder: "Select all or select one by id"
-      }).then(async pickedOption => {
 
-        let dummyPath = `/${selectedItem}`;
-        if (pickedOption === "get one") {
-          await vscode.window.showInputBox({
-            prompt: "Choose an id"
-          }).then(id => {
-            dummyPath = `${dummyPath}/${id}`;
-          });
-        }
+        const pickOptions = [
+          `get all`,
+          `get one`
+        ];
 
-        vscode.window.withProgress({
-          location: vscode.ProgressLocation.Notification,
-          title: `Request to dummyjson.com`,
-          cancellable: false
-        }, async (progress, token) => {
-          progress.report({
-            increment: 0
-          });
+        /**
+         * Displays a quick pick menu to select an option.
+         */
+        vscode.window.showQuickPick(pickOptions, {
+          placeHolder: "Select all or select one by id"
+        }).then(async pickedOption => {
+          if (pickedOption) {
 
-          const options = {
-            hostname: 'dummyjson.com',
-            path: dummyPath,
-            method: 'GET'
-          };
 
-          const req = https.request(options, (res) => {
-            let data = '';
-
-            res.on('data', (chunk) => {
-              data += chunk;
-            });
-
-            res.on('end', () => {
-              const json = JSON.parse(data);
-              progress.report({
-                increment: 100,
-                message: `Done`
+            let dummyPath = `/${selectedItem}`;
+            if (pickedOption === "get one") {
+              await vscode.window.showInputBox({
+                prompt: "Choose an id"
+              }).then(id => {
+                dummyPath = `${dummyPath}/${id}`;
               });
-              resolve(JSON.stringify(json, null, 4));
+            }
+
+            vscode.window.withProgress({
+              location: vscode.ProgressLocation.Notification,
+              title: `Request to dummyjson.com`,
+              cancellable: false
+            }, async (progress, token) => {
+              progress.report({
+                increment: 0
+              });
+
+              const options = {
+                hostname: 'dummyjson.com',
+                path: dummyPath,
+                method: 'GET'
+              };
+
+              const req = https.request(options, (res) => {
+                let data = '';
+
+                res.on('data', (chunk) => {
+                  data += chunk;
+                });
+
+                res.on('end', () => {
+                  const json = JSON.parse(data);
+                  progress.report({
+                    increment: 100,
+                    message: `Done`
+                  });
+                  resolve(JSON.stringify(json, null, 4));
+                });
+              });
+
+              req.on('error', (error) => {
+                _log(error);
+                reject(error);
+              });
+
+              req.end();
             });
-          });
 
-          req.on('error', (error) => {
-            _log(error);
-            reject(error);
-          });
-
-          req.end();
+          }
         });
-      });
+
+      }
     }).catch(err => {
       _log(err);
     });
