0ad1f8c41be3768cc56e6a54f9b7df30aaf1b888
MATHIAS Mickael <mickael.mathias@ext.europarl.europa.eu>
2023-06-28 22:04:37 +0200

    Add tree view

---
+++ extension/src/ScriptTreeItem.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+import { ScriptFile } from "./types";
+
+/**
+ * Represents a script tree item in the tree view.
+ */
+export class ScriptTreeItem extends vscode.TreeItem {
+    /**
+     * The context value for the tree item.
+     */
+    contextValue?: string | undefined = "ScriptTreeItem";
+
+    /**
+     * The associated script file.
+     */
+    script?: ScriptFile;
+
+    /**
+     * Creates a new instance of ScriptTreeItem.
+     * @param name The name of the script.
+     * @param script The associated script file.
+     * @param collapsibleState The collapsible state of the tree item.
+     */
+    constructor(name: string, script?: ScriptFile, collapsibleState?: vscode.TreeItemCollapsibleState) {
+        super(name, collapsibleState);
+        this.script = script;
+    }
+}
