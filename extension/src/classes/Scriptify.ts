fd73ceded31091d0076218286b1a5d9b6a814077
Jordan Skousen <jotheman21@gmail.com>
2023-07-17 21:56:37 -0600

    Ignore `getWorkspaceFolder` errors in Scriptify class

--- extension/src/classes/Scriptify.ts
+++ extension/src/classes/Scriptify.ts
@@ -2,7 +2,7 @@ import { getWorkspaceFolder, getVersion } from "../utils";
 
 export class Scriptify {
 
-  workspaceFolder = getWorkspaceFolder();
+  workspaceFolder = getWorkspaceFolder(true);
 
   /** Current extension version */
   version = getVersion();
